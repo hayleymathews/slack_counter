00f68906ce3ece8167a216c0095ff03a7af58b7c
Hayley Mathews <hayleyemathews@gmail.com>
2018-09-06 20:38:17 -0700

    dont show command

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import time
 import shlex
+import requests
 from datetime import datetime
 from collections import defaultdict
 from flask import Flask, request, jsonify
@@ -29,21 +30,23 @@ def counter():
     team, user = post_data.get('team_id'), post_data['user_id']
     name = post_data.get('text')
     private_channel = post_data.get('channel_name') == 'directmessage'
+    response_url = post_data['response_url']
 
     if name:
         rec = db['counter_db'].counts.find_one({'name': name, 'team': team}, {'_id': 0, 'count': 1, 'log': 1})
 
         if not rec:
-            return jsonify({'text': '*{}* counter does not exist'.format(name),
-                            'response_type': 'in_channel',
-                            'mrkdwn': True})
+            payload = {'text': '*{}* counter does not exist'.format(name),
+                       'response_type': 'in_channel',
+                       'mrkdwn': True}
+            return requests.post(response_url, json=payload)
 
-        count = rec['count']
-        last_updates = '\n'.join(['`{:+.0f}` by <@{}> {}'.format(dtls['val'], dtls['user'], format_date(date))
-                                  for date, dtls in sorted(rec['log'].items(), reverse=True)[:5]])
-        return jsonify({'text': '*{}* count: `{:.0f}`  \n {}'.format(name, count, last_updates),
-                        'response_type': 'in_channel',
-                        'mrkdwn': True})
+        payload = {'text': '*{}* count: `{:.0f}`  \n {}'.format(name, rec['count'], last_updates),
+                   'response_type': 'in_channel',
+                   'attachments': [{'text': '`{:+.0f}` by <@{}> {}'.format(dtls['val'], dtls['user'], format_date(date))}
+                                   for date, dtls in sorted(rec['log'].items(), reverse=True)[:5]]
+                   'mrkdwn': True}
+                   return requests.post(response_url, json=payload)
 
     if private_channel:
         recs = list(db['counter_db'].counts.find({'$or': [{'hidden': {'$ne': True}},
@@ -54,13 +57,15 @@ def counter():
         recs = list(db['counter_db'].counts.find({'hidden': {'$ne': True}, 'team': team}, {'name': 1}))
 
     if not recs:
-        return jsonify({'text': 'No counters created yet',
-                        'response_type': 'in_channel',
-                        'mrkdwn': True})
-
-    return jsonify({'text': '\n'.join(['*{}*'.format(rec['name']) if not rec.get('hidden') else '_{}_'.format(rec['name']) for rec in recs]),
-                    'response_type': 'in_channel',
-                    'mrkdwn': True})
+        payload = {'text': 'No counters created yet',
+                   'response_type': 'in_channel',
+                   'mrkdwn': True}
+        return requests.post(response_url, json=payload)
+    
+    payload = {'text': '\n'.join(['*{}*'.format(rec['name']) if not rec.get('hidden') else '_{}_'.format(rec['name']) for rec in recs]),
+               'response_type': 'in_channel',
+               'mrkdwn': True}
+    return requests.post(response_url, json=payload)
 
 
 @app.route('/counter/incr', methods=['POST'])
@@ -72,12 +77,14 @@ def incr_counter():
     val = float(try_index(text, 1, 1))
     hidden = try_index(text, 2, False) == 'hidden'
     locked = try_index(text, 3, False) == 'locked'
+    response_url = post_data['response_url']
 
     rec = db['counter_db'].counts.find_one({'name': name, 'team': team}, {'_id': 0, 'creator': 1, 'locked': 1})
     if rec and rec.get('locked') and rec['creator'] != user:
-        return jsonify({'text': 'Can not increment a locked counter you do not own',
-                        'response_type': 'in_channel',
-                        'mrkdwn': True})
+        payload = {'text': 'Can not increment a locked counter you do not own',
+                   'response_type': 'in_channel',
+                   'mrkdwn': True}
+        return requests.post(response_url, json=payload)
 
     db['counter_db'].counts.update_one({'name': name, 'team': team},
                                        {'$inc': {'count': val},
@@ -86,9 +93,10 @@ def incr_counter():
                                        upsert=True)
 
     new_count = db['counter_db'].counts.find_one({'name': name, 'team': team}, {'_id': 0, 'count': 1})['count']
-    return jsonify({'text': '`{:+.0f}` for: *{}* \n *{}* count: `{:.0f}`'.format(val, name, name, new_count),
-                    'response_type': 'in_channel',
-                    'mrkdwn': True})
+    payload = {'text': '`{:+.0f}` for: *{}* \n *{}* count: `{:.0f}`'.format(val, name, name, new_count),
+               'response_type': 'in_channel',
+               'mrkdwn': True}
+    return requests.post(response_url, json=payload)
 
 
 @app.route('/counter/del', methods=['POST'])
@@ -96,17 +104,21 @@ def delete_counter():
     post_data = request.form.to_dict()
     team, user = post_data.get('team_id'), post_data['user_id']
     name = post_data.get('text')
+    response_url = post_data['response_url']
 
-    rec = db['counter_db'].counts.find_one({'name': name, 'team': team}, {'_id': 0, 'creator': 1})
+    rec = db['counter_db'].counts.find_one(
+        {'name': name, 'team': team}, {'_id': 0, 'creator': 1})
     if rec['creator'] != user:
-        return jsonify({'text': 'Can not delete a counter you did not create',
-                        'response_type': 'in_channel',
-                        'mrkdwn': True})
+        payload = {'text': 'Can not delete a counter you did not create',
+                   'response_type': 'in_channel',
+                   'mrkdwn': True}
+        return requests.post(response_url, json=payload)
 
     db['counter_db'].counts.delete_one({'name': name, 'team': team})
-    return jsonify({'text': '*{}* counter deleted'.format(name),
-                    'response_type': 'in_channel',
-                    'mrkdwn': True})
+    payload = {'text': '*{}* counter deleted'.format(name),
+               'response_type': 'in_channel',
+               'mrkdwn': True}
+    return requests.post(response_url, json=payload)
 
 
 if __name__ == '__main__':
